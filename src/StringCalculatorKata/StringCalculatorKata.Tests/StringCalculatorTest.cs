bd94599a64d43afe7a02778e05dead2ba2aeb0e8
Christophe Moinard <cmoinard@lucca.fr>
2022-09-20 11:47:34 +0200

    [RED] PropertyBasedTesting sur le nombre unique

--- src/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTest.cs
+++ src/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace StringCalculatorKata.Tests;
 
 public class StringCalculatorTest
 {
+    private static Random Random { get; } = new();
+
     [Fact]
     public void EmptyString()
     {
@@ -12,10 +17,16 @@ public class StringCalculatorTest
         Assert.Equal(0, actual);
     }
 
+    public static TheoryData<string, int> OneNumberData =
+        Enumerable.Range(1, 100)
+            .Select(_ => Random.Next())
+            .ToTheoryData(
+                number => number.ToString(),
+                number => number);
+
     [Theory]
-    [InlineData("3", 3)]
-    [InlineData("14", 14)]
-    public void OnlyOneNumberString(string input, int expected)
+    [MemberData(nameof(OneNumberData))]
+    public void WhenOneNumber_ShouldReturnSameNumber(string input, int expected)
     {
         var actual = StringCalculator.Add(input);
 
@@ -30,5 +41,26 @@ public class StringCalculatorTest
         var actual = StringCalculator.Add(input);
 
         Assert.Equal(expected, actual);
+    }
+}
+
+public static class TheoryDataExtensions
+{
+    public static TheoryData<T1, T2> ToTheoryData<T, T1, T2>(
+        this IEnumerable<T> source,
+        Func<T, T1> getT1,
+        Func<T, T2> getT2
+    )
+    {
+        var theoryData = new TheoryData<T1, T2>();
+        foreach (var value in source)
+        {
+            theoryData.Add(
+                getT1(value),
+                getT2(value)
+            );
+        }
+
+        return theoryData;
     }
 }
